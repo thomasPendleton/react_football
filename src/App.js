6374465b539ef074e2b17153e0e6a9c60d4bc71b
Thomas Pendleton <thomas6576@yahoo.com>
2022-09-28 21:20:00 -0700

    starting new project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import styled from "styled-components"
+import Container from "./components/Container"
+import Header from "./components/Header"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Wrapper>
+      <Header />
+      <Container />
+    </Wrapper>
+  )
 }
 
-export default App;
+const Wrapper = styled.div``
+
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from "react"
+import styled from "styled-components"
+import { IoMdAmericanFootball } from "react-icons/io"
+
+const Header = () => {
+  return (
+    <Wrapper>
+      <div className="logo">
+        <IoMdAmericanFootball />
+      </div>
+      <div className="title">
+        <h3>FootBall Defense Tracker</h3>
+      </div>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.header`
+  display: flex;
+  gap: 10px;
+  background-color: #85c742;
+  padding: 20px 10px;
+  h3 {
+    margin: 0;
+  }
+  .logo{
+    margin: auto 0;
+    transform: rotate(30deg)
+  }
+`
+
+export default Header
