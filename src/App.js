199baa44db4c37f08e9b37bff474940f8ec30321
Thomas Pendleton <thomas6576@yahoo.com>
2022-10-04 10:01:32 -0700

    chang size of several team icons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { useEffect } from "react"
+import React, { useEffect, Suspense } from "react"
 import styled from "styled-components"
 import Container from "./components/Container"
 import Header from "./components/Header"
 import { getGameSchedule } from "./features/stats/statsSlice"
 import { useSelector, useDispatch } from "react-redux"
+
 function App() {
   const { week } = useSelector((store) => store.stats)
   const dispatch = useDispatch()
-  
+
   useEffect(() => {
     dispatch(getGameSchedule())
   }, [])
@@ -22,7 +23,6 @@ function App() {
 
 const Wrapper = styled.div`
   /* background-color: #bdd9c921; */
-
 `
 
 export default App
