c398fb87d51129ba135ce89437a21fab943f595a
Thomas Pendleton <thomas6576@yahoo.com>
2022-11-16 10:47:13 -0800

    add no condition to weather icon and snow icon

--- src/components/WeatherIcon.js
+++ src/components/WeatherIcon.js
@@ -13,56 +13,68 @@ import {
 import styled from "styled-components"
 
 const WeatherIcon = ({ forecastDescription, city }) => {
-    // console.log(city, forecastDescription)
-  if (forecastDescription === "Scattered Clouds") {
-    return (
-      <Wrapper>
-        <TiWeatherCloudy className="icon" />
-      </Wrapper>
-    )
-  }
-  if (forecastDescription === "Heavy Intensity Rain" || forecastDescription === 'Moderate Rain') {
-    return (
-      <Wrapper>
-        <TiWeatherDownpour style={{ color: "#0066b2" }} className="icon" />
-      </Wrapper>
-    )
-  }
-  if (forecastDescription === "Clear Sky" || forecastDescription === 'Few Clouds') {
-    return (
-      <Wrapper>
-        <TiWeatherSunny style={{ color: "yellow" }} className="icon" />
-      </Wrapper>
-    )
-  }
-  if (forecastDescription === "Broken Clouds") {
-    return (
-      <Wrapper>
-        <TiWeatherPartlySunny style={{ color: "lightGray" }} className="icon" />
-      </Wrapper>
-    )
-  }
-  if (forecastDescription === "Light Rain") {
-    return (
-      <Wrapper>
-        <TiWeatherShower style={{ color: "lightBlue" }} className="icon" />
-      </Wrapper>
-    )
-  }
-  if (forecastDescription === "Overcast Clouds") {
-    return (
-      <Wrapper>
-        <TiWeatherCloudy className="icon" />
-      </Wrapper>
-    )
-  }
-  if (forecastDescription === "Snow") {
-    return (
-      <Wrapper>
-        <TiWeatherSnow className="icon" />
-      </Wrapper>
-    )
-  }
+    console.log(city, forecastDescription)
+    if (forecastDescription === null) {
+      return console.log("null")
+    }
+    if (forecastDescription === "Scattered Clouds") {
+      return (
+        <Wrapper>
+          <TiWeatherCloudy className="icon" />
+        </Wrapper>
+      )
+    }
+    if (
+      forecastDescription === "Heavy Intensity Rain" ||
+      forecastDescription === "Moderate Rain"
+    ) {
+      return (
+        <Wrapper>
+          <TiWeatherDownpour style={{ color: "#0066b2" }} className="icon" />
+        </Wrapper>
+      )
+    }
+    if (
+      forecastDescription === "Clear Sky" ||
+      forecastDescription === "Few Clouds"
+    ) {
+      return (
+        <Wrapper>
+          <TiWeatherSunny style={{ color: "yellow" }} className="icon" />
+        </Wrapper>
+      )
+    }
+    if (forecastDescription === "Broken Clouds") {
+      return (
+        <Wrapper>
+          <TiWeatherPartlySunny
+            style={{ color: "lightGray" }}
+            className="icon"
+          />
+        </Wrapper>
+      )
+    }
+    if (forecastDescription === "Light Rain") {
+      return (
+        <Wrapper>
+          <TiWeatherShower style={{ color: "lightBlue" }} className="icon" />
+        </Wrapper>
+      )
+    }
+    if (forecastDescription === "Overcast Clouds") {
+      return (
+        <Wrapper>
+          <TiWeatherCloudy className="icon" />
+        </Wrapper>
+      )
+    }
+    if (forecastDescription.includes("Snow")) {
+      return (
+        <Wrapper>
+          <TiWeatherSnow className="icon" />
+        </Wrapper>
+      )
+    }
 }
 
 const Wrapper = styled.div`
